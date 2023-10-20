104d8b49466eefec572b03c6cb760d6eb7f48bd0
amenallah lahmer <92236016+amenlahmer@users.noreply.github.com>
2023-10-20 20:22:27 +0100

    Update OperatorServiceImplTest.java

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -79,7 +79,7 @@ class OperatorServiceImplTest {
 
     @Test
     void deleteOperator() {
-        Long operatorId = 3L; // The ID of the operator to delete
+        Long operatorId = 1L; // The ID of the operator to delete
         Mockito.when(operatorRepository.findById(operatorId)).thenReturn(Optional.of(operator));
         
         ArgumentCaptor<Operator> operatorCaptor = ArgumentCaptor.forClass(Operator.class);
