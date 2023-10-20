53c75aa6809fa2d7cd7be0a27e6533194e7e23d8
aboubaker007 <92956754+aboubaker007@users.noreply.github.com>
2023-10-20 19:55:59 +0100

    aboubaker's changes

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.devops_project.entities.Product;
