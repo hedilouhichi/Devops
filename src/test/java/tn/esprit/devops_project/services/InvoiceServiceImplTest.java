3dc2f70425a0db71be37a18d5a3a0b833b274a1c
Esya Saker <esya.saker@esprit.tn>
2023-11-05 08:50:03 +0100

    update pom.xml

--- src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -1,36 +1,30 @@
 package tn.esprit.devops_project.services;
 
-import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.*;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.devops_project.entities.Invoice;
+import tn.esprit.devops_project.repositories.InvoiceRepository;
 import tn.esprit.devops_project.services.Iservices.IInvoiceService;
 
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-class InvoiceServiceImplTest {
+public class InvoiceServiceImplTest {
     @Autowired
     IInvoiceService iinvoiceService;
-    @Test
-    @Order(0)
-    void retrieveAllInvoices() {
-
-        List<Invoice> invoiceDetails = iinvoiceService.retrieveAllInvoices();
-        Assertions.assertEquals( 0,invoiceDetails.size());
-    }
 
     @Test
-    void cancelInvoice() {
+    public void retrieveAllInvoices() {
+       List<Invoice> invoiceList = iinvoiceService.retrieveAllInvoices();
+       Assertions.assertEquals(0,invoiceList.size());
+
     }
-
-
-
-
-
 }
