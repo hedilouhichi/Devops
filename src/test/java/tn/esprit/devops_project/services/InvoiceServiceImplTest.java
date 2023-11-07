c2af110bf5dfaeabf2a9706f7385f7292bcb2f75
Esya Saker <esya.saker@esprit.tn>
2023-11-07 13:30:31 +0100

    tests

---
+++ src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -0,0 +1,13 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InvoiceServiceImplTest {
+
+    @Test
+    public void retrieveAllInvoices() {
+
+    }
+}
